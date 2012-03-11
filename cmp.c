227f922fd5815be8934043281a3d522abfa7f89e
Conrado Mader Blanco <cmaderblanco@gmail.com>
2012-03-11 19:54:13 -0300

    fedetrol

--- cmp.c
+++ cmp.c
@@ -4,3 +4,8 @@ int cmpTeam(void* team1, void* team2)
 {
     return ((team_t*)team1)->points-((team_t*)team2)->points;
 }
+
+int cmpSportist(void* sport1, void* sport2)
+{
+    return strcmp((sportist_t*)sport1->name, (sportist_t*)sport2->name);
+}
