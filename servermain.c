2a177c814f0481a7cdda76c0c61fa18ed89ae0c7
Conrado Mader Blanco <cmaderblanco@gmail.com>
2012-03-17 16:13:58 -0300

    asasas

--- servermain.c
+++ servermain.c
@@ -5,39 +5,44 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include "list.h"
+#include <pthread.h>
+#include "server.h"
 #include "league.h"
+
 
 int main()
 {
-	listADT users, leagues, teams, trades, connectedUsers;
-	/*loadUsers(users);
-	loadLeagues(leagues);
-	loadTrades(trades);
-	loadTeam(teams);
-*/
-	//thread q controle los q se conectan
-	int fdsend, fdrcv;
-	char buffer[5];
-	user_t * user1= malloc(sizeof(user_t));
-	strcpy(user1->name, "caco");
-	strcpy(user1->password, "cuca");
-	//insert(users, user1);
-	mkfifo("./fifos",0666);
-	fdrcv=open("./fifoc", O_RDONLY);
-	fdsend=open("./fifos", O_WRONLY);
-	read(fdrcv, buffer, 5);
-	if(strcmp(user1->name, buffer)==0){
-		printf("usuario encontrado\n");
-		read(fdrcv, buffer, 5);
-		if(strcmp(user1->password, buffer)==0){
-			printf("login correcto\n");
-			strcpy(buffer, "si");
-			write(fdsend, buffer, 5);
+	pthread_t thread;
+
+
+	return 0;
+}
+
+void * listenClient()
+{
+	while(1)
+	{
+		int type;
+		char name[NAME_LENGTH], password[NAME_LENGTH];
+
+		rcvMsg(type);
+		rcvMsg(name);
+		rcvMsg(password);
+		if(type==LOGIN)
+		{
+			if(logIn(name,password)!=0)
+			{
+				sendMsg(aux);
+			}
+		}
+		else if(type==SIGNUP)
+		{
+			if(signUp(name, password)!=0)
+			{
+				sendMsg(aux);
+			}
+			logIn(name, password);
 		}
 
 	}
-	close(fdsend);
-	close(fdrcv);
-
-	return 0;
 }
