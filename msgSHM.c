3d7cc399e9da0520fe72421f864688a8823445b6
framundo <feedeee@hotmail.com>
2012-03-25 22:25:23 -0300

    shm

--- msgSHM.c
+++ msgSHM.c
@@ -1,54 +1,139 @@
 #include "msg.h"
-#define SIZE 100
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <errno.h>
 
-int sndMsg(int fd, void* data, int size)
+#define SIZE 1000
+
+static sem_t* initMutex(char* id);
+static void enter(sem_t* sem);
+static void leave(sem_t* sem);
+
+typedef struct
 {
-	
+	sem_t* sem;
+	void* mem;
+} shm_t;
+
+int sndMsg(void* fd, void* data, int size)
+{
+	int i;
+	shm_t *shm=(shm_t*)fd;
+	enter(shm->sem);
+	int *head=(int*)shm->mem;
+	for(i=0;i<size;i++)
+	{
+		((char*)shm->mem)[*head]=((char*)data)[i];
+		*head++;
+		if(*head>=SIZE)
+		{
+			*head=sizeof(int)*2;
+		}
+	}
+	leave(shm->sem);
+	return size;
 }
 
-int rcvMsg(int fd, void* data, int size)
+int rcvMsg(void* fd, void* data, int size)
 {
-	
+	int i;
+	shm_t *shm=(shm_t*)fd;
+	enter(shm->sem);
+	int *head=(int*)shm->mem;
+	int *tail=(int*)(shm->mem+sizeof(int));
+	for(i=0;i<size;i++)
+	{
+		if(*tail==*head)
+		{
+			leave(shm->sem);
+			return i;
+		}
+		((char*)data)[i]=((char*)shm->mem)[*tail];
+		*tail++;
+		if(*tail>=SIZE)
+		{
+			*tail=sizeof(int)*2;
+		}
+	}
+	leave(shm->sem);
 }
 
-int connectChannel(char* id, int flag)
+void* connectChannel(char* id, int flag)
 {
 	char shmName[10];
+	int fd;
+	shm_t* shm=malloc(sizeof(shm_t));
+	
 	sprintf(shmName,"/shm%s",id);
-	int fd=shm_open(shmName, flag|O_CREAT, 0666));
-	return (int)mmap(NULL, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	fd=shm_open(shmName, O_RDWR|O_CREAT, 0666);
+	shm->sem=initMutex(id);
+	ftruncate(fd, SIZE);
+	shm->mem=mmap(NULL, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	close(fd);
+	if(shm->mem==MAP_FAILED)
+	{
+		printf("Mapping failure: %d\n",errno);
+		exit(-1);
+	}
+	((int*)shm->mem)[0]=sizeof(int)*2;
+	((int*)shm->mem)[1]=sizeof(int)*2;
+	return (void*)shm;
 }
 
-int rcvString(int fd, char* data)
+int rcvString(void* fd, char* data)
 {
-	
+	int i=0;
+	shm_t *shm=(shm_t*)fd;
+	enter(shm->sem);
+	int *head=(int*)shm->mem;
+	int *tail=(int*)(shm->mem+sizeof(int));
+	while(*head!=*tail)
+	{
+		data[i]=((char*)shm->mem)[*tail];
+		*tail++;
+		if(data[i]==0)
+		{
+			leave(shm->sem);
+			return i+1;
+		}
+		if(*tail>=SIZE)
+		{
+			*tail=sizeof(int)*2;
+		}
+		i++;
+	}
+	leave(shm->sem);
+	return i;
 }
 
-int sndString(int fd, char* string)
+int sndString(void* fd, char* string)
 {
-	
+	return sndMsg(fd,string,strlen(string));
 }
 
-void disconnect(int fd)
+void disconnect(void* fd)
 {
-	munmap((void*)fd,SIZE);
+	shm_t *shm=(shm_t*)fd;
+	munmap(shm->mem,SIZE);
+	sem_close(shm->sem);
 }
 
-void
-initmutex(void)
+static sem_t* initMutex(char* id)
 {
-	if ( !(sd = sem_open("/mutex", O_RDWR|O_CREAT, 0666, 1)) )
-		fatal("sem_open");
+	char semName[10];
+	sprintf(semName,"/mutex%s",id);
+	return sem_open(semName, O_RDWR|O_CREAT, 0666, 1);
 }
 
-void
-enter(void)
+static void enter(sem_t* sem)
 {
-	sem_wait(sd);
+	sem_wait(sem);
 }
 
-void
-leave(void)
+static void leave(sem_t* sem)
 {
-	sem_post(sd);
+	sem_post(sem);
 }
