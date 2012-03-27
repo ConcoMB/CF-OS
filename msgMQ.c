b254e2a785c2a7b088eca4a4e3d8e6c4ff293542
framundo <feedeee@hotmail.com>
2012-03-27 15:09:01 -0300

    MQ

--- msgMQ.c
+++ msgMQ.c
@@ -3,7 +3,7 @@
 #include <errno.h>
 #include <sys/msg.h>
 
-#define MQ_MSGSIZE 30
+#define MQ_MSGSIZE 5
 
 typedef struct
 {
@@ -13,7 +13,7 @@ typedef struct
 
 typedef struct
 {
-	long fromID;
+	long int fromID;
 	char data[MQ_MSGSIZE];
 } msg_t;
 
@@ -29,7 +29,7 @@ int sndMsg(void* fd, void* data, int size)
 	{
 		printf("Send Error: errno %d\n", errno);
 	}
-	printf("Sent with id: %i (%d)\n",(int)msg.fromID,i);
+	//printf("Sent with id: %i (%d)\n",(int)msg.fromID,i);
 	return i;
 }
 
@@ -37,9 +37,9 @@ int rcvMsg(void* fd, void* data, int size)
 {
 	mq_t* mq=(mq_t*) fd;
 	msg_t msg;
-	printf("Recieving with id: %d...",mq->id);
-	int i= msgrcv(mq->mqd, &msg, MQ_MSGSIZE, mq->id,0);
-	printf("%d\n",i);
+	//printf("Recieving with id: %d...",mq->id);
+	int i= msgrcv(mq->mqd, &msg, (size_t)(sizeof(msg)), mq->id,0);
+	//printf("%d (errno: %d)\n",i, errno);
 	strncpy((char*)data, msg.data, size);
 	return i;
 }
@@ -53,7 +53,7 @@ void createChannel(int id)
 		printf("Error creating Message Queue with key: %d - errno: %d\n", key, errno);
 		exit(-1);
 	}
-	printf("Created queue %d\n", key);
+	//printf("Created queue %d\n", key);
 }
 
 void* connectChannel(int id)
@@ -68,7 +68,7 @@ void* connectChannel(int id)
 		printf("Error connecting to Message Queue with key %d - errno: %d\n", key, errno);
 		exit(-1);
 	}
-	printf("Connected to queue %d\n",key);
+	//printf("Connected to queue %d\n",key);
 	return (void*)mq;
 }
 
@@ -76,7 +76,7 @@ int rcvString(void* fd, char* data)
 {
 	mq_t* mq=(mq_t*) fd;
 	msg_t msg;
-	int i=msgrcv(mq->mqd, &msg, MQ_MSGSIZE, mq->id,0);
+	int i=msgrcv(mq->mqd, &msg, (size_t)(sizeof(msg)), mq->id,0);
 	strcpy((char*)data, msg.data);
 	return i;
 }
